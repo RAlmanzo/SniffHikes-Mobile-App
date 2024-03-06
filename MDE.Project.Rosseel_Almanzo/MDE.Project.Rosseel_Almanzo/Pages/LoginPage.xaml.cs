d3020e45bc8b48205e7b83846f882492f96c37c9
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-03-06 10:29:00 +0100

    Add bindingcontext to bind loginviewmodel

--- MDE.Project.Rosseel_Almanzo/MDE.Project.Rosseel_Almanzo/Pages/LoginPage.xaml.cs
+++ MDE.Project.Rosseel_Almanzo/MDE.Project.Rosseel_Almanzo/Pages/LoginPage.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using MDE.Project.Rosseel_Almanzo.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,9 +14,13 @@ namespace MDE.Project.Rosseel_Almanzo.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LoginPage : ContentPage
     {
+        private LoginViewModel _viewModel;
+
         public LoginPage()
         {
             InitializeComponent();
+            _viewModel = new LoginViewModel();
+            BindingContext = _viewModel;
         }
     }
 }
