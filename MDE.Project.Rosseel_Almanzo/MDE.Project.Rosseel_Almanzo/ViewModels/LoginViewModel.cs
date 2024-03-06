2ca4b994f41ef8a0a235fb27c148533a2fd9679d
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-03-06 10:36:40 +0100

    Create folder viewmodels and create login and registerviewmodel

---
+++ MDE.Project.Rosseel_Almanzo/MDE.Project.Rosseel_Almanzo/ViewModels/LoginViewModel.cs
@@ -0,0 +1,36 @@
+﻿using MDE.Project.Rosseel_Almanzo.Pages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MDE.Project.Rosseel_Almanzo.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new Command(async() =>
+                {
+                    await Application.Current.MainPage.Navigation.PushAsync(new MainPage());
+                });
+            }
+        }
+
+        public ICommand RegisterCommand
+        {
+            get
+            {
+                return new Command(async () =>
+                {
+                    await Application.Current.MainPage.Navigation.PushAsync(new RegisterPage());
+                });
+            }
+        }
+    }
+}
---
+++ MDE.Project.Rosseel_Almanzo/MDE.Project.Rosseel_Almanzo/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MDE.Project.Rosseel_Almanzo.ViewModels
+{
+    public class RegisterViewModel
+    {
+    }
+}
