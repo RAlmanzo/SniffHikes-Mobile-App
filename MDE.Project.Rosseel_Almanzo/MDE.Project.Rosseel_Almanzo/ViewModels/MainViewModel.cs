a9829d57b01379e918269d91e3d871505aa7afed
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-03-09 19:54:19 +0100

    Create new icommand eventscommand

--- MDE.Project.Rosseel_Almanzo/MDE.Project.Rosseel_Almanzo/ViewModels/MainViewModel.cs
+++ MDE.Project.Rosseel_Almanzo/MDE.Project.Rosseel_Almanzo/ViewModels/MainViewModel.cs
@@ -2,10 +2,22 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace MDE.Project.Rosseel_Almanzo.ViewModels
 {
     public class MainViewModel : FreshBasePageModel
     {
+        public ICommand EventsCommand
+        {
+            get
+            {
+                return new Command(async () =>
+                {
+                    await CoreMethods.PushPageModel<EventsViewModel>();
+                });
+            }
+        }
     }
 }
