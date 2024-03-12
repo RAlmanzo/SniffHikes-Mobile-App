b76f59c8ac609a77f58595a201f58337187d6749
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-03-12 22:27:02 +0100

    Set up properties and commands for flyoutpage in mainviewmodel

--- MDE.Project.Rosseel_Almanzo/MDE.Project.Rosseel_Almanzo/ViewModels/MainViewModel.cs
+++ MDE.Project.Rosseel_Almanzo/MDE.Project.Rosseel_Almanzo/ViewModels/MainViewModel.cs
@@ -1,4 +1,7 @@
 ﻿using FreshMvvm;
+using MDE.Project.Rosseel_Almanzo.Domain.Models;
+using MDE.Project.Rosseel_Almanzo.Domain.Services;
+using MDE.Project.Rosseel_Almanzo.Domain.Services.Mock;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,6 +12,48 @@ namespace MDE.Project.Rosseel_Almanzo.ViewModels
 {
     public class MainViewModel : FreshBasePageModel
     {
+        private readonly IFlyoutMenuService _flyoutMenuService;
+        private List<FlyoutPageItem> items;
+
+        public List<FlyoutPageItem> Items
+        {
+            get => items;
+            set => items = value;
+        }
+
+        public MainViewModel()
+        {
+            _flyoutMenuService = new MockFlyoutMenuService();
+        }
+
+        public override void Init(object initData)
+        {
+            base.Init(initData);
+            RefreshData.Execute(null);
+        }
+
+        public ICommand RefreshData
+        {
+            get
+            {
+                return new Command(async () =>
+                {
+                    List<FlyoutPageItem> fetchedItems = await _flyoutMenuService.GetItems();
+                    Items = new List<FlyoutPageItem>(fetchedItems);
+                });
+            }
+        }
+
+        public ICommand HomeCommand
+        {
+            get
+            {
+                return new Command(async () =>
+                {
+                    await CoreMethods.PushPageModel<MainViewModel>();
+                });
+            }
+        }
         public ICommand EventsCommand
         {
             get
