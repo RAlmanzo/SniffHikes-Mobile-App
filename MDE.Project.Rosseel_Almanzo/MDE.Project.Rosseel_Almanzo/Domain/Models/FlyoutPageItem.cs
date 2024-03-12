6d76970e5c99819977a89e3b58ec5524865ca16c
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-03-12 22:16:37 +0100

    Create flyoutpageitem model

---
+++ MDE.Project.Rosseel_Almanzo/MDE.Project.Rosseel_Almanzo/Domain/Models/FlyoutPageItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MDE.Project.Rosseel_Almanzo.Domain.Models
+{
+    public class FlyoutPageItem
+    {
+        public string Title { get; set; }
+        public string IconSource { get; set; }
+        public Type TargetPage {  get; set; }
+    }
+}
