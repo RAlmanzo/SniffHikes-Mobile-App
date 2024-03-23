7d35a20aa253cc1bcc1abfb8a3bca1fb0b27b59e
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-03-23 22:15:06 +0100

    Create new viewmodel createrouteviewmodel

---
+++ MDE.Project.Rosseel_Almanzo/MDE.Project.Rosseel_Almanzo/ViewModels/CreateRouteViewModel.cs
@@ -0,0 +1,11 @@
+﻿using FreshMvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MDE.Project.Rosseel_Almanzo.ViewModels
+{
+    internal class CreateRouteViewModel : FreshBasePageModel
+    {
+    }
+}
