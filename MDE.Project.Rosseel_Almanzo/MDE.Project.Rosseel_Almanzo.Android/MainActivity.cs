4b30b6ee38bd4c5efdcd65239e6c2e74740ab7e1
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-03-29 15:09:42 +0100

    Add icon for android

--- MDE.Project.Rosseel_Almanzo/MDE.Project.Rosseel_Almanzo.Android/MainActivity.cs
+++ MDE.Project.Rosseel_Almanzo/MDE.Project.Rosseel_Almanzo.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace MDE.Project.Rosseel_Almanzo.Droid
 {
-    [Activity(Label = "MDE.Project.Rosseel_Almanzo", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "MDE.Project.Rosseel_Almanzo", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
