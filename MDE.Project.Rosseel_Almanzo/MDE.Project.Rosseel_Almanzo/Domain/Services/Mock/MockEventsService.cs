238fa3084e7b97cc5876a7ac92b40d78bf011e67
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-03-30 15:59:20 +0100

    Add images and comments to all events

--- MDE.Project.Rosseel_Almanzo/MDE.Project.Rosseel_Almanzo/Domain/Services/Mock/MockEventsService.cs
+++ MDE.Project.Rosseel_Almanzo/MDE.Project.Rosseel_Almanzo/Domain/Services/Mock/MockEventsService.cs
@@ -50,6 +50,21 @@ namespace MDE.Project.Rosseel_Almanzo.Domain.Services.Mock
                     City = "Oostende",
                     Country = "België",
                     DateEvent = DateTime.Now.AddDays(14),
+                    Images = new List<Image>
+                    {
+                        new Image{Title="first image", ImagePath ="AFreedomPath"},
+                        new Image{Title="first image", ImagePath ="AFreedomPath"},
+                        new Image{Title="first image", ImagePath ="AFreedomPath"},
+                        new Image{Title="Second image", ImagePath="https://source.unsplash.com/user/c_v_r/1900x800"}
+                    },
+                    Comments = new List<Comment>
+                    {
+                        new Comment { Content = "Ik vond het honden event echt geweldig! Mijn hond en ik hebben genoten van de leuke activiteiten en de gezellige sfeer. Hopelijk volgend jaar weer!" },
+                        new Comment { Content = "Het honden event was zo leuk! Mijn harige vriend heeft nieuwe vrienden gemaakt en we hebben samen veel plezier gehad. Bedankt aan iedereen die het heeft georganiseerd!" },
+                        new Comment { Content = "Dit honden event was precies wat mijn viervoeter nodig had. Hij heeft zoveel plezier gehad en we hebben allebei genoten van de dag vol activiteiten en leuke ontmoetingen!" },
+                        new Comment { Content = "Het honden event was een geweldige ervaring! Mijn hond en ik hebben genoten van de gezelligheid en de leuke dingen om te doen. Een fantastische dag voor ons beiden!" },
+                        new Comment { Content = "Wat een fantastisch honden event! Ik heb zoveel leuke mensen ontmoet en mijn hond heeft zich geen moment verveeld. Bedankt aan iedereen die heeft geholpen om dit mogelijk te maken!" },
+                    }
                 },
                 new Event
                 {
@@ -60,6 +75,21 @@ namespace MDE.Project.Rosseel_Almanzo.Domain.Services.Mock
                     City = "Eupen",
                     Country = "België",
                     DateEvent = DateTime.Now.AddDays(21),
+                    Images = new List<Image>
+                    {
+                        new Image{Title="first image", ImagePath ="AFreedomPath"},
+                        new Image{Title="first image", ImagePath ="AFreedomPath"},
+                        new Image{Title="first image", ImagePath ="AFreedomPath"},
+                        new Image{Title="Second image", ImagePath="https://source.unsplash.com/user/c_v_r/1900x800"}
+                    },
+                    Comments = new List<Comment>
+                    {
+                        new Comment { Content = "Ik vond het honden event echt geweldig! Mijn hond en ik hebben genoten van de leuke activiteiten en de gezellige sfeer. Hopelijk volgend jaar weer!" },
+                        new Comment { Content = "Het honden event was zo leuk! Mijn harige vriend heeft nieuwe vrienden gemaakt en we hebben samen veel plezier gehad. Bedankt aan iedereen die het heeft georganiseerd!" },
+                        new Comment { Content = "Dit honden event was precies wat mijn viervoeter nodig had. Hij heeft zoveel plezier gehad en we hebben allebei genoten van de dag vol activiteiten en leuke ontmoetingen!" },
+                        new Comment { Content = "Het honden event was een geweldige ervaring! Mijn hond en ik hebben genoten van de gezelligheid en de leuke dingen om te doen. Een fantastische dag voor ons beiden!" },
+                        new Comment { Content = "Wat een fantastisch honden event! Ik heb zoveel leuke mensen ontmoet en mijn hond heeft zich geen moment verveeld. Bedankt aan iedereen die heeft geholpen om dit mogelijk te maken!" },
+                    }
                 },
                 new Event
                 {
@@ -70,13 +100,33 @@ namespace MDE.Project.Rosseel_Almanzo.Domain.Services.Mock
                     City = "Turnhout",
                     Country = "België",
                     DateEvent = DateTime.Now.AddDays(28),
+                    Images = new List<Image>
+                    {
+                        new Image{Title="first image", ImagePath ="AFreedomPath"},
+                        new Image{Title="first image", ImagePath ="AFreedomPath"},
+                        new Image{Title="first image", ImagePath ="AFreedomPath"},
+                        new Image{Title="Second image", ImagePath="https://source.unsplash.com/user/c_v_r/1900x800"}
+                    },
+                    Comments = new List<Comment>
+                    {
+                        new Comment { Content = "Ik vond het honden event echt geweldig! Mijn hond en ik hebben genoten van de leuke activiteiten en de gezellige sfeer. Hopelijk volgend jaar weer!" },
+                        new Comment { Content = "Het honden event was zo leuk! Mijn harige vriend heeft nieuwe vrienden gemaakt en we hebben samen veel plezier gehad. Bedankt aan iedereen die het heeft georganiseerd!" },
+                        new Comment { Content = "Dit honden event was precies wat mijn viervoeter nodig had. Hij heeft zoveel plezier gehad en we hebben allebei genoten van de dag vol activiteiten en leuke ontmoetingen!" },
+                        new Comment { Content = "Het honden event was een geweldige ervaring! Mijn hond en ik hebben genoten van de gezelligheid en de leuke dingen om te doen. Een fantastische dag voor ons beiden!" },
+                        new Comment { Content = "Wat een fantastisch honden event! Ik heb zoveel leuke mensen ontmoet en mijn hond heeft zich geen moment verveeld. Bedankt aan iedereen die heeft geholpen om dit mogelijk te maken!" },
+                    }
                 },
             };
         }
 
-        public async Task<List<Event>> GetAllEvents()
+        public async Task<List<Event>> GetAllEventsAsync()
         {
             return await Task.FromResult(_events);
+        }
+
+        public Task<List<Event>> GetAllEventsByUserId(int id)
+        {
+            throw new NotImplementedException();
         }
 
         public async Task<Event> GetEventByIdAsync(int id)
@@ -84,10 +134,10 @@ namespace MDE.Project.Rosseel_Almanzo.Domain.Services.Mock
             return await Task.FromResult(_events.FirstOrDefault(e => e.Id == id));
         }
 
-        public async Task<List<Image>> GetEventImagesAsync(int id)
-        {
-            var selectedEvent = await GetEventByIdAsync(id);
-            return (List<Image>)await Task.FromResult(selectedEvent.Images);
-        }
+        //public async Task<List<Image>> GetEventImagesAsync(int id)
+        //{
+        //    var selectedEvent = await GetEventByIdAsync(id);
+        //    return (List<Image>)await Task.FromResult(selectedEvent.Images);
+        //}
     }
 }
