2a2542fca3b8da4b1a2e4e5572b160885eababb4
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-04-28 11:22:16 +0200

    Create eventsservice and init firebase realtime db

---
+++ MDE.Project.Rosseel_Almanzo/MDE.Project.Rosseel_Almanzo/Domain/Services/Mock/EventsService.cs
@@ -0,0 +1,102 @@
+﻿using Firebase.Database;
+using Firebase.Database.Query;
+using MDE.Project.Rosseel_Almanzo.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDE.Project.Rosseel_Almanzo.Domain.Services.Mock
+{
+    public class EventsService : IEventsService
+    {
+        private static List<Event> _events;
+        private readonly FirebaseClient _client;
+
+        public EventsService()
+        {
+            _client = new FirebaseClient("https://sniffhikes-8e9a6-default-rtdb.europe-west1.firebasedatabase.app/");
+        }
+
+        public async Task<bool> CreateEventAsync(Event newEvent)
+        {
+            try
+            {
+                //_events.Add(newEvent);
+                await _client.Child("Events")
+                    .PostAsync(newEvent);
+                return await Task.FromResult(true);
+            }
+            catch
+            {
+                return await Task.FromResult(false);
+            }
+        }
+
+        public async Task<bool> AddCommentAsync(string id, Comment comment)
+        {
+            try
+            {
+                var existingEvent = await GetEventByIdAsync(id);
+                if (existingEvent.Comments == null)
+                {
+                    existingEvent.Comments = new ObservableCollection<Comment>();
+                }
+                existingEvent.Comments.Add(comment);
+
+                await _client.Child("Events").Child(id).PutAsync(existingEvent);
+                return await Task.FromResult(true);
+            }
+            catch
+            {
+                return await Task.FromResult(false);
+            }
+        }
+
+        public async Task<List<BaseModel<Event>>> GetAllEventsAsync()
+        {
+            var eventsSnapshot = await _client.Child("Events").OnceAsync<Event>();
+
+            var eventsList = new List<BaseModel<Event>>();
+
+            foreach (var snapshot in eventsSnapshot)
+            {
+                var eventData = new BaseModel<Event>
+                {
+                    Key = snapshot.Key,
+                    Value = snapshot.Object,
+                };
+
+                eventsList.Add(eventData);
+            }
+
+            return eventsList;
+        }
+
+
+        public async Task<List<Event>> GetAllEventsByUserId(int id)
+        {
+            return await Task.FromResult(_events.Where(e => e.OrginazerId == id).ToList());
+        }
+
+        public async Task<Event> GetEventByIdAsync(string id)
+        {
+            var eventSnapshot = await _client.Child("Events").Child(id).OnceSingleAsync<Event>();
+
+            if (eventSnapshot != null)
+            {
+                return await Task.FromResult(eventSnapshot);
+            }
+            return null;
+            //return await Task.FromResult(_events.FirstOrDefault(e => e.Id == id));
+        }
+
+        //public async Task<List<Image>> GetEventImagesAsync(int id)
+        //{
+        //    var selectedEvent = await GetEventByIdAsync(id);
+        //    return (List<Image>)await Task.FromResult(selectedEvent.Images);
+        //}
+    }
+}
