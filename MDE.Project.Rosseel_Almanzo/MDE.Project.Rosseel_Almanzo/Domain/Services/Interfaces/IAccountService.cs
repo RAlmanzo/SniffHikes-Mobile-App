63d90d3e23a44d9183efde4fc1a4eb7096966867
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-04-28 13:25:21 +0200

    Create interface iaccountservice

---
+++ MDE.Project.Rosseel_Almanzo/MDE.Project.Rosseel_Almanzo/Domain/Services/Interfaces/IAccountService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDE.Project.Rosseel_Almanzo.Domain.Services.Interfaces
+{
+    public interface IAccountService
+    {
+        Task<string> Login(string username, string password);
+    }
+}
