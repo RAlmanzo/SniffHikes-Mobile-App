d2e76e8ca10b54621cd2036dd8bd15a4f03fe900
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-04-28 16:33:32 +0200

    Change id type to string

--- MDE.Project.Rosseel_Almanzo/MDE.Project.Rosseel_Almanzo/Domain/Models/User.cs
+++ MDE.Project.Rosseel_Almanzo/MDE.Project.Rosseel_Almanzo/Domain/Models/User.cs
@@ -6,7 +6,7 @@ namespace MDE.Project.Rosseel_Almanzo.Domain.Models
 {
     public class User
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
