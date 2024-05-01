58d63d190cd6ffde1a7e2ca3afe367bc0d77147c
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-05-01 23:13:38 +0200

    Create eventdto

---
+++ MDE.Project.Rosseel_Almanzo/MDE.Project.Rosseel_Almanzo/Infrastructure/Dtos/EventDto.cs
@@ -0,0 +1,21 @@
+﻿using MDE.Project.Rosseel_Almanzo.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MDE.Project.Rosseel_Almanzo.Infrastructure.Dtos
+{
+    public class EventDto
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public IEnumerable<Image> Images { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public DateTime DateEvent { get; set; }
+        public ICollection<Comment> Comments { get; set; }
+        public string OrginazerId { get; set; }
+    }
+}
