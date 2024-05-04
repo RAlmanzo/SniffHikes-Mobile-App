e089c460f480b2157ccbf8d12894b75a3316e639
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-05-04 16:20:15 +0200

    Create routedto

---
+++ MDE.Project.Rosseel_Almanzo/MDE.Project.Rosseel_Almanzo/Infrastructure/Dtos/RouteDto.cs
@@ -0,0 +1,21 @@
+﻿using MDE.Project.Rosseel_Almanzo.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MDE.Project.Rosseel_Almanzo.Infrastructure.Dtos
+{
+    public class RouteDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public IEnumerable<Image> Images { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public DateTime DateEvent { get; set; }
+        public IEnumerable<Comment> Comments { get; set; }
+        public string OrganizerId { get; set; }
+    }
+}
