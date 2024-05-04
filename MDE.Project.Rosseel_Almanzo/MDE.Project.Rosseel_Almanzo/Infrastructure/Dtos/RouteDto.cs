a9f4738b14942a086291488fcc47a46af99894a4
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-05-04 18:05:43 +0200

    Set property comments to icollection

--- MDE.Project.Rosseel_Almanzo/MDE.Project.Rosseel_Almanzo/Infrastructure/Dtos/RouteDto.cs
+++ MDE.Project.Rosseel_Almanzo/MDE.Project.Rosseel_Almanzo/Infrastructure/Dtos/RouteDto.cs
@@ -15,7 +15,7 @@ namespace MDE.Project.Rosseel_Almanzo.Infrastructure.Dtos
         public string City { get; set; }
         public string Country { get; set; }
         public DateTime DateEvent { get; set; }
-        public IEnumerable<Comment> Comments { get; set; }
+        public ICollection<Comment> Comments { get; set; }
         public string OrganizerId { get; set; }
     }
 }
