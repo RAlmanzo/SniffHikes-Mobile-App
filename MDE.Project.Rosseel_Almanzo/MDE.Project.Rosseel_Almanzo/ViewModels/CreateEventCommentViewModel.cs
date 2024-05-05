f8bf31d52c6595174704e5658862fe76d4289a6e
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-05-05 16:38:30 +0200

    Create viewmodel createeventcommentviewmodel

---
+++ MDE.Project.Rosseel_Almanzo/MDE.Project.Rosseel_Almanzo/ViewModels/CreateEventCommentViewModel.cs
@@ -0,0 +1,85 @@
+﻿using FreshMvvm;
+using MDE.Project.Rosseel_Almanzo.Domain.Models;
+using MDE.Project.Rosseel_Almanzo.Domain.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MDE.Project.Rosseel_Almanzo.ViewModels
+{
+    public class CreateEventCommentViewModel : FreshBasePageModel
+    {
+        private readonly IEventsService _eventsService;
+
+        private string comment;
+        private string id;
+
+        public string Id
+        {
+            get => id;
+            set
+            {
+                id = value;
+                RaisePropertyChanged(nameof(Comment));
+            }
+        }
+
+        public string Comment
+        {
+            get => comment;
+            set
+            {
+                comment = value;
+                RaisePropertyChanged(nameof(Comment));
+            }
+        }
+
+        public CreateEventCommentViewModel(IEventsService eventsService)
+        {
+            _eventsService = eventsService;
+        }
+
+        public override void Init(object initData)
+        {
+            base.Init(initData);
+
+            Id = initData.ToString();
+        }
+
+        public ICommand AddCommentCommand
+        {
+            get
+            {
+                return new Command(async () =>
+                {
+                    var newComment = new Comment
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        CreatedOn = DateTime.Now,
+                        Content = comment,
+                    };
+
+                    if (!await _eventsService.AddCommentAsync(id, newComment))
+                    {
+                        await CoreMethods.DisplayAlert("Error", "Something went wrong and comment could not be added", "Ok");
+                    };
+
+                    await CoreMethods.PushPageModel<EventDetailViewModel>(id, false, true);
+                });
+            }
+        }
+
+        public ICommand CancelCommand
+        {
+            get
+            {
+                return new Command(async () =>
+                {
+                    await CoreMethods.PushPageModel<EventDetailViewModel>(id, false, true);
+                });
+            }
+        }
+    }
+}
