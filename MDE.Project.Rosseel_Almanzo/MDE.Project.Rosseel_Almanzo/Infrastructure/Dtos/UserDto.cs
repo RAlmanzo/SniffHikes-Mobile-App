f39f47444e8d5a4cf1fc41cd8504442854226dcc
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-05-05 20:04:22 +0200

    Create userdto

---
+++ MDE.Project.Rosseel_Almanzo/MDE.Project.Rosseel_Almanzo/Infrastructure/Dtos/UserDto.cs
@@ -0,0 +1,21 @@
+﻿using MDE.Project.Rosseel_Almanzo.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MDE.Project.Rosseel_Almanzo.Infrastructure.Dtos
+{
+    public class UserDto
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Password { get; set; }
+        public IEnumerable<Dog> Dogs { get; set; }
+    }
+}
