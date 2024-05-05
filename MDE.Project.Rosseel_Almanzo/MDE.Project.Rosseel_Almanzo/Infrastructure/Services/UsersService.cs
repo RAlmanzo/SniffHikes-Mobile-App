a4571cdc5befc83cf16746d398b6e2be144ac039
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-05-05 20:05:48 +0200

    Refacter method getuserbyidasync and edit code to delete user account

--- MDE.Project.Rosseel_Almanzo/MDE.Project.Rosseel_Almanzo/Infrastructure/Services/UsersService.cs
+++ MDE.Project.Rosseel_Almanzo/MDE.Project.Rosseel_Almanzo/Infrastructure/Services/UsersService.cs
@@ -2,10 +2,14 @@
 using Firebase.Database.Query;
 using MDE.Project.Rosseel_Almanzo.Domain.Models;
 using MDE.Project.Rosseel_Almanzo.Domain.Services;
+using MDE.Project.Rosseel_Almanzo.Infrastructure.Dtos;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 
 namespace MDE.Project.Rosseel_Almanzo.Infrastructure.Services
 {
@@ -34,14 +38,49 @@ namespace MDE.Project.Rosseel_Almanzo.Infrastructure.Services
             }
         }
 
+        public async Task<string> DeleteUserAsync(string id)
+        {
+            try
+            {
+                //TODO delete images from db!!!!!!!!!!!!!!!!!!!!!!!
+
+                await _client.Child("Users").Child(id).DeleteAsync();
+                return await Task.FromResult("Deleted");
+            }
+            catch (Exception ex)
+            {
+                return await Task.FromResult(ex.Message);
+            }
+        }
+
         public Task<List<User>> GetAllUsersAsync()
         {
             throw new NotImplementedException();
         }
 
-        public Task<User> GetUserByIdAsync(int id)
+        public async Task<User> GetUserByIdAsync(string id)
         {
-            throw new NotImplementedException();
+            var userSnapshot = await _client.Child("Users").Child(id).OnceSingleAsync<UserDto>();
+
+            if (userSnapshot != null)
+            {
+                var user = new User
+                {
+                    Id = id,
+                    FirstName = userSnapshot.FirstName,
+                    LastName = userSnapshot.LastName,
+                    Email = userSnapshot.Email,
+                    City = userSnapshot.City,
+                    Country = userSnapshot.Country,
+                    Gender = userSnapshot.Gender,
+                    DateOfBirth = userSnapshot.DateOfBirth,
+                    Password = userSnapshot.Password,
+                    Dogs = userSnapshot.Dogs != null ? new ObservableCollection<Dog>(userSnapshot.Dogs) : new ObservableCollection<Dog>(),
+                };
+                
+                return await Task.FromResult(user);
+            };
+            return null;
         }
     }
 }
