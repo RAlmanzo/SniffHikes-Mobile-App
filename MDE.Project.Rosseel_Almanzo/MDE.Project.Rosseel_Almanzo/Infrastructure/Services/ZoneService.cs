bfc86903129420864dda5c13b4d332ff9c773849
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-06-08 12:15:36 +0200

    Create zoneservice class

---
+++ MDE.Project.Rosseel_Almanzo/MDE.Project.Rosseel_Almanzo/Infrastructure/Services/ZoneService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MDE.Project.Rosseel_Almanzo.Infrastructure.Services
+{
+    public class ZoneService
+    {
+    }
+}
