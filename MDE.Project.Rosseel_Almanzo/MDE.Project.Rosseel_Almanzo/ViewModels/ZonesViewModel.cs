9af567dfde139cb106268c4350b58def14456ed4
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-06-09 15:19:37 +0200

    Add command gottoupdatepage and createzoneommand and refacter code

--- MDE.Project.Rosseel_Almanzo/MDE.Project.Rosseel_Almanzo/ViewModels/ZonesViewModel.cs
+++ MDE.Project.Rosseel_Almanzo/MDE.Project.Rosseel_Almanzo/ViewModels/ZonesViewModel.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Windows.Input;
 using Xamarin.Forms;
 using MDE.Project.Rosseel_Almanzo.Domain.Services.Interfaces;
+using Xamarin.Essentials;
 
 namespace MDE.Project.Rosseel_Almanzo.ViewModels
 {
@@ -15,19 +16,34 @@ namespace MDE.Project.Rosseel_Almanzo.ViewModels
     {
         private readonly IZonesService _zonesService;
 
-        private ObservableCollection<Zone> zones;
+        private ObservableCollection<BaseModel> zones;
         private ObservableCollection<Domain.Models.Image> images;
-        private Zone selectedZone;
+        private BaseModel selectedZone;
+        private string id;
 
-        public Zone SelectedZone
+        public string Id
+        {
+            get => id;
+            set => id = value;
+        }
+
+        public BaseModel SelectedZone
         {
             get => selectedZone;
             set
             {
                 selectedZone = value;
-
-                // RaisePropertyChanged(nameof(SelectedItem));
-                GoToDetailPage.Execute(null);
+                if (value != null)
+                {
+                    if (selectedZone.OrginazerId == id)
+                    {
+                        GoToUpdatePage.Execute(null);
+                    }
+                    else
+                    {
+                        GoToDetailPage.Execute(null);
+                    }
+                }
             }
         }
 
@@ -41,7 +57,7 @@ namespace MDE.Project.Rosseel_Almanzo.ViewModels
             }
         }
 
-        public ObservableCollection<Zone> Zones
+        public ObservableCollection<BaseModel> Zones
         {
             get => zones;
             set
@@ -51,14 +67,20 @@ namespace MDE.Project.Rosseel_Almanzo.ViewModels
             }
         }
 
-        public ZonesViewModel()
+        public ZonesViewModel(IZonesService zonesService)
         {
-            _zonesService = new MockZonesService();
+            _zonesService = zonesService;
         }
 
         public override void Init(object initData)
         {
             base.Init(initData);
+            //RefreshData.Execute(null);
+        }
+
+        protected override void ViewIsAppearing(object sender, EventArgs e)
+        {
+            base.ViewIsAppearing(sender, e);
             RefreshData.Execute(null);
         }
 
@@ -68,9 +90,10 @@ namespace MDE.Project.Rosseel_Almanzo.ViewModels
             {
                 return new Command(async () =>
                 {
-                    //List<Domain.Models.Image> images = await _eventsService.GetEventImagesByEventIdAsync();
-                    List<Zone> fetchedEvents = await _zonesService.GetAllZonesAsync();
-                    Zones = new ObservableCollection<Zone>(fetchedEvents);
+                    Id = await SecureStorage.GetAsync("token");
+
+                    var fetchedEvents = await _zonesService.GetAllZonesAsync();
+                    Zones = new ObservableCollection<BaseModel>(fetchedEvents);
                 });
             }
         }
@@ -83,6 +106,29 @@ namespace MDE.Project.Rosseel_Almanzo.ViewModels
                 {
                     if (SelectedZone != null)
                         await CoreMethods.PushPageModel<ZoneDetailsViewModel>(SelectedZone.Id, false, true);
+                });
+            }
+        }
+
+        public ICommand GoToUpdatePage
+        {
+            get
+            {
+                return new Command(async () =>
+                {
+                    if (selectedZone != null)
+                        await CoreMethods.PushPageModel<UpdateZoneViewModel>(selectedZone.Id, false, true);
+                });
+            }
+        }
+
+        public ICommand CreateZoneCommand
+        {
+            get
+            {
+                return new Command(async () =>
+                {
+                    await CoreMethods.PushPageModel<CreateZoneViewModel>();
                 });
             }
         }
