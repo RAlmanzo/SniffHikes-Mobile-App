6c7d0fa31b80e019158773b615d596db738b44d3
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-06-10 20:33:17 +0200

    Edit code to delete selected image

--- MDE.Project.Rosseel_Almanzo/MDE.Project.Rosseel_Almanzo/Infrastructure/Services/ImageService.cs
+++ MDE.Project.Rosseel_Almanzo/MDE.Project.Rosseel_Almanzo/Infrastructure/Services/ImageService.cs
@@ -1,8 +1,10 @@
 ﻿using Firebase.Storage;
+using MDE.Project.Rosseel_Almanzo.Domain.Models;
 using MDE.Project.Rosseel_Almanzo.Domain.Services.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Essentials;
@@ -65,30 +67,31 @@ namespace MDE.Project.Rosseel_Almanzo.Domain.Services
             catch (Exception ex)
             {
                 return ex.Message;
-                //Console.WriteLine($"CapturePhotoAsync THREW: {ex.Message}");
             }
         }
+
         private async Task<string> LoadPhotoAsync(FileResult photo)
         {
-            // canceled
             if (photo == null)
             {
                 PhotoPath = null;
                 return "error";
             }
-            ////save image in firebase storage
-            //var task = new FirebaseStorage("sniffhikes-8e9a6.appspot.com").Child("Events").Child(photo.FileName).PutAsync(await photo.OpenReadAsync());
-            //PhotoPath = await task;
+
             var result = _storageClient.Child("Events").Child(photo.FileName).PutAsync(await photo.OpenReadAsync());
             PhotoPath = await result;
             return PhotoPath;
-            //// save the file into local storage
-            //var newFile = Path.Combine(FileSystem.CacheDirectory, photo.FileName);
-            //using (var stream = await photo.OpenReadAsync())
-            //using (var newStream = File.OpenWrite(newFile))
-            //    await stream.CopyToAsync(newStream);
+        }
 
-            //PhotoPath = newFile;
+        public async Task<bool> DeleteImage(Image image)
+        {
+            var uri = new Uri(image.ImagePath);
+            var path = uri.AbsolutePath;
+            var decodedPath = Uri.UnescapeDataString(path);
+            var fileName = decodedPath.Split('/').Last();
+
+            await _storageClient.Child("Events").Child(fileName).DeleteAsync();
+            return await Task.FromResult(true);
         }
     }
 }
