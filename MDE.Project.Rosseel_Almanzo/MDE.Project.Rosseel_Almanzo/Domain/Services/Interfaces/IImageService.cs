689e6d8859763bc0946dc55cd79c305a59d37978
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-06-10 20:33:01 +0200

    Add method deleteimage

--- MDE.Project.Rosseel_Almanzo/MDE.Project.Rosseel_Almanzo/Domain/Services/Interfaces/IImageService.cs
+++ MDE.Project.Rosseel_Almanzo/MDE.Project.Rosseel_Almanzo/Domain/Services/Interfaces/IImageService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MDE.Project.Rosseel_Almanzo.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +10,6 @@ namespace MDE.Project.Rosseel_Almanzo.Domain.Services.Interfaces
     {
         Task<string> TakePhotoAsync();
         Task<string> PickPhotoAsync();
+        Task<bool> DeleteImage(Image image);
     }
 }
