0189f36a3e4c2081cbd48b469f1e30ed4b47f412
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-06-10 18:37:24 +0200

    Create commentvalidator

---
+++ MDE.Project.Rosseel_Almanzo/MDE.Project.Rosseel_Almanzo/Domain/Services/Validators/CommentsValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using MDE.Project.Rosseel_Almanzo.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MDE.Project.Rosseel_Almanzo.Domain.Services.Validators
+{
+    public class CommentsValidator : AbstractValidator<Comment>
+    {
+        public CommentsValidator()
+        {
+            RuleFor(x => x.Content)
+                .NotEmpty()
+                .WithMessage("Content is required")
+                .MinimumLength(2)
+                .WithMessage("Content is to short")
+                .MaximumLength(300)
+                .WithMessage("Content is to long");
+        }
+    }
+}
