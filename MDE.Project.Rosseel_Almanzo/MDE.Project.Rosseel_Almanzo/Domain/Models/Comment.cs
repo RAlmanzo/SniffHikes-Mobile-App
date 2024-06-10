0847d1e807984f846f554e4490741dd165288333
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-06-10 12:43:37 +0200

    Add property username

--- MDE.Project.Rosseel_Almanzo/MDE.Project.Rosseel_Almanzo/Domain/Models/Comment.cs
+++ MDE.Project.Rosseel_Almanzo/MDE.Project.Rosseel_Almanzo/Domain/Models/Comment.cs
@@ -7,5 +7,7 @@ namespace MDE.Project.Rosseel_Almanzo.Domain.Models
         public string Id { get; set; }
         public DateTime CreatedOn { get; set;}
         public string Content { get; set; }
+        public string UserId { get; set; }
+        public string UserName { get; set; }
     }
 }
